09c46c56b968634e28263fd95526797bc567dafc
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2021-12-16 20:50:32 +0530

    changes

--- code1-project1/create_tables.py
+++ code1-project1/create_tables.py
@@ -6,8 +6,12 @@ cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,username text,password text)"
 
+
+
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name test,price real)"
+cursor.execute(create_table)
 connection.commit()
 
 connection.close()
